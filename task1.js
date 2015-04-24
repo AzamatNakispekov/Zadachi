a824789029115e8e085a043b08cfcb163607522b
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-24 19:30:23 +0600

    1
    1

--- task1.js
+++ task1.js
@@ -1,4 +1,4 @@
-var mass = [3,2,5,7,4];
+var mass = [3,2,5,7,4,9];
 var i;
 var sum=0;
 
@@ -7,7 +7,7 @@ console.log(mass)
 
 
 console.log('summary massiv');
-for(i=0;i<=5;i++) {
+for(i=0;i<=mass[i];i++) {
     sum=sum+mass[i];
     console.log(sum)
 }
---
+++ task2.js
@@ -0,0 +1,14 @@
+var mass = [7,6,5,4,3];
+var i;
+var res=0;
+
+console.log('массив')
+
+    console.log( mass );
+
+console.log('массив с вычисленной разницей')
+for(i=0;i<=5;i++) 
+{
+res=mass[i+1]-[i];
+    console.log(res);
+}
