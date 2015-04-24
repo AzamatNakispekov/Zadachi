0cfafe91c9c057f9590f8b6ff7d2aa4cecc7d0be
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-24 18:48:36 +0600

    1
    задание 1

---
+++ task1.js
@@ -0,0 +1,15 @@
+var mass=[]
+
+   var i;
+   var sum=0;
+
+console.log('массив')
+
+     for(i=0;i<=5;i++){
+      console.log( i )}
+
+console.log('сложенный массив')
+
+      for(i=0;i<=5;i++){
+       sum=sum+i;
+      console.log(sum)}
