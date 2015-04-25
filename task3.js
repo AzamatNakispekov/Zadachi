7422c19d85743c09fab357b85bd5a28248fb1538
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-26 02:21:17 +0600

    2,3
    2.3

--- task3.js
+++ task3.js
@@ -1,8 +1,8 @@
 
 var s,r,d,u;
-var namber1=12;
+var namber1=18;
 console.log("ваше первое число=",namber1);
-var namber2=12;
+var namber2=19;
 console.log("ваше второе число=",namber2);
 s=namber1+namber2;
 console.log('сумма=',s);
@@ -12,3 +12,19 @@ d=namber1/namber2;
 console.log('деление=',d);
 u=namber1*namber2;
 console.log('умножение=',u)
+var max= namber1;
+
+if(namber1>namber2)
+{
+
+    console.log("Число",namber1,"больше","числа",namber2);
+}
+else 
+if(namber2==namber1){
+
+    console.log("Число",namber1,"и число",namber2," являются равными")
+
+}
+
+else {
+console.log("Число",namber2,"больше","числа",namber1);}
