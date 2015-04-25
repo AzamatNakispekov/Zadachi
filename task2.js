e1e10665fc6e598377e8fd4090ff23d996db0dbd
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-26 01:43:29 +0600

    2,3
    2,3

--- task2.js
+++ task2.js
@@ -1,4 +1,4 @@
-var mass = [7,6,5,4,3];
+var mass = [17,6,15,8,2,20];
 var i;
 var res=0;
 
@@ -7,8 +7,8 @@ console.log('массив')
     console.log( mass );
 
 console.log('массив с вычисленной разницей')
-for(i=0;i<=5;i++) 
+for(i=0;i<= mass.length ;i++) 
 {
-res=mass[i+1]-[i];
+res=(mass[i+1]-mass[i]);
     console.log(res);
 }
---
+++ task3.js
@@ -0,0 +1,14 @@
+
+var s,r,d,u;
+var namber1=12;
+console.log("ваше первое число=",namber1);
+var namber2=12;
+console.log("ваше второе число=",namber2);
+s=namber1+namber2;
+console.log('сумма=',s);
+r=namber1-namber2;
+console.log('разность=',r);
+d=namber1/namber2;
+console.log('деление=',d);
+u=namber1*namber2;
+console.log('умножение=',u)
