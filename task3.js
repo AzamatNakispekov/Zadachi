bf7e3653345c3e1b432c0d7281e404c79625acb5
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-27 18:10:17 +0600

    3
    3

--- task3.js
+++ task3.js
@@ -4,36 +4,39 @@ var read = require('read');
 
 
 read({ prompt : 'Введите первое число'}, function (err, namber) {
-  read({ prompt : 'Введите второе число'}, function (err, namberr) {
-   process.stdin.destroy();
+read({ prompt : 'Введите второе число'}, function (err, namberr) {
+    process.stdin.destroy();
+    
 console.log('Введенное вами первое число =',namber,'Веденное вами второе число =',namberr);
+    
 var a = Number(namber);
       var b = Number(namberr);
+    
+    
 s= a + b ;
-console.log('сумма=',s);
+ console.log('сумма=',s);
 r=a-b;
-console.log('разность=',r);
+ console.log('разность=',r);
 d=a/b;
-console.log('деление=',d);
+ console.log('деление=',d);
 u=a*b;
-console.log('умножение=',u)
+ console.log('умножение=',u)
 
 
 var max= a;
 
 if(a>b)
 {
-
-    console.log("Число",a,"больше","числа",b);
+  console.log("Число",a,"больше","числа",b);
 }
 else 
-if(b==a){
-
-    console.log("Число",a,"и число",b," являются равными")
-
+if(b==a)
+{
+  console.log("Число",a,"и число",b," являются равными")
 }
 
-else {
-console.log("Число",b,"больше","числа",a);}
-  });
+else 
+{
+   console.log("Число",b,"больше","числа",a);}
+});
 })
