b7b4d14ca159ca1ce465d6db1160a7e4979f1f51
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-28 18:48:25 +0600

    12
    12

--- task3.js
+++ task3.js
@@ -1,4 +1,4 @@
-var s,r,d,u,max= a;
+var s,r,d,u;
 var read = require('read');
 
 read({ prompt : 'Введите первое число'}, function (err, n) {
@@ -19,7 +19,7 @@ d=a/b;
   console.log('деление=',d);
 u=a*b;
   console.log('умножение=',u)
-
+var max= a;
 if(a>b){
   console.log("Число",a,"больше","числа",b); }
     else
