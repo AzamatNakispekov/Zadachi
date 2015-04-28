19c1c5494226e399c7a69deb96c3c264ea647bd6
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-04-28 18:19:49 +0600

    Task3

--- task3.js
+++ task3.js
@@ -1,42 +1,30 @@
-
-var s,r,d,u;
+var s,r,d,u,max= a;
 var read = require('read');
 
+read({ prompt : 'Введите первое число'}, function (err, n) {
+read({ prompt : 'Введите второе число'}, function (err, n2) {
 
-read({ prompt : 'Введите первое число'}, function (err, namber) {
-read({ prompt : 'Введите второе число'}, function (err, namberr) {
     process.stdin.destroy();
     
-console.log('Введенное вами первое число =',namber,'Веденное вами второе число =',namberr);
+console.log('Введенное вами первое число =',n,'Веденное вами второе число =',n2);
     
-var a = Number(namber);
-      var b = Number(namberr);
-    
-    
+  var a = parseFloat(n);
+  var b = parseFloat(n2);
+
 s= a + b ;
- console.log('сумма=',s);
+  console.log('сумма=',s);
 r=a-b;
- console.log('разность=',r);
+  console.log('разность=',r);
 d=a/b;
- console.log('деление=',d);
+  console.log('деление=',d);
 u=a*b;
- console.log('умножение=',u)
+  console.log('умножение=',u)
 
-
-var max= a;
-
-if(a>b)
-{
-  console.log("Число",a,"больше","числа",b);
-}
-else 
-if(b==a)
-{
-  console.log("Число",a,"и число",b," являются равными")
-}
-
-else 
-{
-   console.log("Число",b,"больше","числа",a);}
+if(a>b){
+  console.log("Число",a,"больше","числа",b); }
+    else
+    if(b==a){
+  console.log("Число",a,"и число",b," являются равными")}
+else {console.log("Число",b,"больше","числа",a);}
 });
 })
