7fbf2ec00225d577c240e62fb1d58350b7d8e3af
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-05-05 19:06:38 +0600

    New task0.js
    Fix all mistake

--- task0.js
+++ task0.js
@@ -1,14 +1,17 @@
-t=20;
+t=10;
 h=200;
 
-
+//x=(g*(t*t))/2;//Какое расстояние пролетит за 20 секунд
+//x1=g*t;//Какая скорость по прошествии 20 секунд
+//t=Math.sqrt(2*h/g);//Сколько длится свободное падение с высоты 200м
+//f=Math.sqrt(2*g*h);//Какова скорость после падения с высоты 200м
 function fix(th){
     
   b=th*10/36;
   return b;}
 
 
-function fall(){
+function fall(t){
 
    x=(g*(t*t))/2;
    return x;}
@@ -31,28 +34,11 @@ function fall2(h){
     return f;} 
 
 
-function allinEarth(){
-console.log ("ЗЕМЛЯ");
- g=9.8;
- var i1=fall(t)
-   console.log("Какое расстояние пролетит за 20 секунд=",i1.toFixed(1),"м");
-
- var i2=speed(t)
-   console.log("Какая скорость по прошествии 20 секунд=",i2.toFixed(1),"м/c","или",fix(speed(i2)).toFixed(1),"км.ч");
-
- var i3=high(h)
-   console.log("Сколько длится свободное падение с высоты 200м=", i3.toFixed(1),"c");  
-    
- var i4=fall2(h)
-   console.log("Какова скорость после падения с высоты 200м=",i4.toFixed(1),"м/c","или",fix(fall2(i4)).toFixed(1),"км/ч");
-}
-allinEarth();
 
 
-function allinMars(){ 
-console.log("МАРС");
- g=3.71;
- var i1=fall(t)
+
+function allinPlanets(planetsName,t,g){ 
+var i1=fall(t)
     console.log("Какое расстояние пролетит за 20 секунд=",i1.toFixed(1),"м");
 
  var i2=speed(t)
@@ -64,6 +50,11 @@ console.log("МАРС");
  var i4=fall2(h)
      console.log("Какова скорость после падения с высоты 200м=",i4.toFixed(1),"м/c","или",fix(fall2(i4)).toFixed(1),"км/ч");
 }
-allinMars();
+ allinPlanets("Zemlia",t=20,g=9.8)
+
+ allinPlanets("Mars",t=20,g=3.71)
+
+ 
+ 
 
 
