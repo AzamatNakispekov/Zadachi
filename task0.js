ea5de4357046ecbd1be30da08197f2d66d47805c
AzamatNakispekov <Azik_nakispekov@mail.ru>
2015-05-05 19:13:03 +0600

    task0.js new 1
    Fix all mistakes, or not

--- task0.js
+++ task0.js
@@ -1,11 +1,8 @@
 t=10;
 h=200;
 
-//x=(g*(t*t))/2;//Какое расстояние пролетит за 20 секунд
-//x1=g*t;//Какая скорость по прошествии 20 секунд
-//t=Math.sqrt(2*h/g);//Сколько длится свободное падение с высоты 200м
-//f=Math.sqrt(2*g*h);//Какова скорость после падения с высоты 200м
-function fix(th){
+
+function Con(th){
     
   b=th*10/36;
   return b;}
@@ -37,22 +34,22 @@ function fall2(h){
 
 
 
-function allinPlanets(planetsName,t,g){ 
+function allinPlanets(planetsName,g){ 
 var i1=fall(t)
     console.log("Какое расстояние пролетит за 20 секунд=",i1.toFixed(1),"м");
 
  var i2=speed(t)
-     console.log("Какая скорость по прошествии 20 секунд=",i2.toFixed(1),"м/c","или",fix(speed(i2)).toFixed(1),"км.ч");
+     console.log("Какая скорость по прошествии 20 секунд=",i2.toFixed(1),"м/c","или",Con(speed(i2)).toFixed(1),"км.ч");
 
  var i3=high(h)
      console.log("Сколько длится свободное падение с высоты 200м=", i3.toFixed(1),"c");  
     
  var i4=fall2(h)
-     console.log("Какова скорость после падения с высоты 200м=",i4.toFixed(1),"м/c","или",fix(fall2(i4)).toFixed(1),"км/ч");
+     console.log("Какова скорость после падения с высоты 200м=",i4.toFixed(1),"м/c","или",Con(fall2(i4)).toFixed(1),"км/ч");
 }
- allinPlanets("Zemlia",t=20,g=9.8)
+ allinPlanets("Zemlia",g=9.8)
 
- allinPlanets("Mars",t=20,g=3.71)
+ allinPlanets("Mars",g=3.71)
 
  
  
